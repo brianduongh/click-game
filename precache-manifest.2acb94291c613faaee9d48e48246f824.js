cc05ae29a11d59f446be2ce2dc21c991ba441e2c
Brian Duong <brianduongh@gmail.com>
2019-01-14 21:45:31 -0600

    Updates

---
+++ precache-manifest.2acb94291c613faaee9d48e48246f824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "451cb8796073a9e8640b",
+    "url": "/click-game/static/js/runtime~main.451cb879.js"
+  },
+  {
+    "revision": "917913161e9d8ea4cfda",
+    "url": "/click-game/static/js/main.91791316.chunk.js"
+  },
+  {
+    "revision": "fd0e2bdb1ed3d46b5087",
+    "url": "/click-game/static/js/1.fd0e2bdb.chunk.js"
+  },
+  {
+    "revision": "917913161e9d8ea4cfda",
+    "url": "/click-game/static/css/main.8b983f05.chunk.css"
+  },
+  {
+    "revision": "d352ffcf7d433e1572810cbc3d0474d5",
+    "url": "/click-game/index.html"
+  }
+];
